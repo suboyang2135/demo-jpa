318573423e32462a27a558ec6df5a8e9549d54d7
yang.xu <yang.xu@ikang.com>
2020-12-17 11:26:07 +0800

    name

--- src/main/java/com/ikang/Application.java
+++ src/main/java/com/ikang/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoJpsApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoJpsApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
